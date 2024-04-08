01a26b9a86d64c5c1830675c87ac91fcc9ef795e
joshuabutler <butlerja.nhs@gmail.com>
2024-04-08 17:13:36 -0400

    create Deck model

---
+++ models/deck.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose"
+
+const Schema = mongoose.Schema
+
+const deckSchema = new Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  wins: {
+    type: Number
+  },
+  losses: {
+    type: Number
+  },
+  notes: {
+    type: String
+  },
+  mana: {
+    type: String,
+    enum: ['White', 'Black', 'Blue', 'Red', 'Green', 'Multi-color'],
+    required: true
+  }, 
+  cards: [{ type: Schema.Types.ObjectId, ref: 'Card' }]
+})
+
+const Deck = mongoose.model('Deck', deckSchema)
+
+export { Deck }
