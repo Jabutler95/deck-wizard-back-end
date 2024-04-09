d23940c32faf11ec8276ea5ecb9bff161ea53511
joshuabutler <butlerja.nhs@gmail.com>
2024-04-09 18:12:09 -0400

    edit card model to match api parameters, add timestamps to both models

--- models/card.js
+++ models/card.js
@@ -3,7 +3,40 @@ import mongoose from 'mongoose'
 const Schema = mongoose.Schema
 
 const cardSchema = new Schema({
-  // Test api to ensure naming conventions match 
+  name: {
+    type: String,
+    required: true
+  },
+  type: {
+    type: String,
+    required: true
+  },
+  image: {
+    type: String,
+    required: true 
+  },
+  text: {
+    type: String,
+    required: true
+  },
+  colorIdentity: {
+    type: String,
+    required: true
+  },
+  colors: {
+    type: String,
+    required: true
+  },
+  manaCost: {
+    type: String,
+    required: true
+  },
+  multiverseid: {
+    type: String,
+    required: true
+  } 
+},{
+    timestamps: true,
 })
 
 const Card = mongoose.model('Card', cardSchema)
